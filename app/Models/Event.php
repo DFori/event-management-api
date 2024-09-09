324bb3a5957dcd797818aa1fe0b5c1350cbe210a
DFori <daniel.tatama@binghamuni.edu.ng>
2024-09-09 15:12:36 +0100

    send emails

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
 
 class Event extends Model
 {
     use HasFactory;
+    use Notifiable;
 
     protected $fillable = ['name', 'description', 'start_time', 'end_time', 'user_id'];
 
